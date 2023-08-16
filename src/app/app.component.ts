95d2940af1e5b09fe314f6dba02296b56f7e9937
AbrahamMzansie <abraham.nkomo@gmail.com>
2023-08-16 13:31:12 +0200

    added a list of houses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
+  imports: [HomeComponent],
+  template: `<main>
+    <header class="brand-name">
+      <img
+        aria-hidden="true"
+        alt="logo"
+        class="brand-logo"
+        src="/assets/logo.svg"
+      />
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'Homes';
 }
